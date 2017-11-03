cfddadba535041e90ba4b88db1c3b56572a04c1a
Blake Baxendell <bbaxendell@consultwebs-email.com>
2017-11-03 11:48:23 -0400

    app update

--- JS/node/lesson_1/app.js
+++ JS/node/lesson_1/app.js
@@ -1,3 +1,29 @@
-	console.log("hello world.");
-	console.error("Ooops");
-	console.dir({name: "Blake", age: 34});
+//Problem: We need a simple way to look at a user's badge count and Javascript points
+const https = require('https');
+const username = "blakebaxendell";
+//Solution: Use node.js to connect to treehouse api to get profile information to print out
+function printMessage(username, badgeCount, points) {
+	const message = `${username} has ${badgeCount} total badge(s) and ${points} points in Javascript`;
+	console.log(message);
+}
+
+//Connect to Treehouse API (https://teamtreehouse.com/username.json)
+function getProfile(username) {
+	const request = https.get(`https://teamtreehouse.com/${username}.json`, (response) => {
+	          // Read the data
+	          let body = '';
+	          response.on('data', (info) => {
+	            body += info.toString();
+	          });
+	          response.on('end', () => {
+	             // Parse the data
+	             const profile = JSON.parse(body);
+	             // Print the data
+	             printMessage(username, profile.badges.length, profile.points.JavaScript);
+	           });
+
+	});
+}
+//console.log(process.argv);
+const users = process.argv.slice(2);
+users.forEach( getProfile);
