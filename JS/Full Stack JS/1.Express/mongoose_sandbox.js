49cad46612402b6f90b4c4c7095d96f2afa9da56
Blake Baxendell <wahhadesign@gmail.com>
2020-03-01 15:44:54 -0500

    no message

--- JS/Full Stack JS/1.Express/mongoose_sandbox.js
+++ JS/Full Stack JS/1.Express/mongoose_sandbox.js
@@ -16,11 +16,15 @@ db.once("open", function(){
 
 	var Schema = mongooose.Schema;
 	var AnimalSchema = new Schema({
-		type: String,
-		color: String,
+		type: {type: String, default: "goldfish"},
 		size: String,
-		mass: Number,
-		name: String
+		color: {type: String, default: "golden"},
+		mass: {type: Number, default: 0.007},
+		name: {type: String, default: "Angela"}
+	});
+
+	AnimalSchema.pre("save", function(next){
+		
 	});
 
 	var Animal = mongoose.model("Animal", AnimalSchema);
@@ -28,11 +32,57 @@ db.once("open", function(){
 	var elephant = new Animal({
 		type: "elephant",
 		color: "gray",
-		size: "big",
 		mass: 6000,
 		name: "Lawrence"
 	});
 
+	var animal = new Animal({}); //Goldfish
+
+	var whale = new Animal({
+		type: "whale",
+		mass: 190500,
+		name: "Fig"
+	});
+
+	var animalData = [
+		{
+			type: "mouse",
+			color: "gray",
+			mass: 0.035,
+			name: "Marvin"
+		},
+		{
+			type: "nutria",
+			color: "brown",
+			mass: 6.35,
+			name: "Gretchen"
+		},
+		{
+			type: "wolf",
+			color: "gray",
+			mass: 45,
+			name: "Iris"
+		},
+		elephant,
+		animal,
+		whale
+	];
+
+	Animal.remove({}, function(err){
+		if (err) console.error(err);
+		Animal.create(animalData, function(err, animals){
+			if (err) console.error(err);
+			Animal.find({size: "big"}, function(err, animals){
+				animals.forEach(function(animal){
+					console.log(animal.name + "the" + animal.color + " " + animal.type);
+				});
+				db.close(function(){
+					console.log("db connection closed");
+				});
+			});
+		});
+	});
+	/*
 	elephant.save(function(err) {
 		if(err) console.error("save failed.", err);
 		else console.log("saved!");
@@ -40,5 +90,5 @@ db.once("open", function(){
 			console.log("db connection closed");
 		});
 	});
-
+	*/
 });
